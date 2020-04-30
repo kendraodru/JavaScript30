c78763ac703519140ef38a3410f727af0162ecf7
Kendra <kmodrunia@ucdavis.edu>
2020-04-29 18:53:54 -0700

    proj11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -34,17 +34,34 @@ function handleRangeUpdate(){
 }
 
 function handleProgress(){
+    const percent = (video.currentTime / video.duration)*100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+    video.currentTime = scrubTime;
 }
 
 //hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(btn=> btn.addEventListener('click', skip))
 
+let rangeMouseDown = true
 ranges.forEach(slider => slider.addEventListener('change', handleRangeUpdate))
 ranges.forEach(slider => slider.addEventListener('mousemove', handleRangeUpdate))
+
+let mouseDown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mouseDown && scrub(e))
+//if mouseDown is true will more on to scrub, if not will return false and wont run scrub
+progress.addEventListener('mousedown',()=> mouseDown = true)
+progress.addEventListener('mouseup', ()=>mouseDown = false)
+
+
